17d7f562b93ae0305694905881af804b872fcc93
emersonreyes727 <emersonreyes727@gmail.com>
2016-11-23 07:36:42 -0800

    Complete "Collecting the Pick Up Objects" video

--- unityP1/Assets/Scripts/playerController.cs
+++ unityP1/Assets/Scripts/playerController.cs
@@ -22,4 +22,13 @@ public class playerController : MonoBehaviour {
 
 		rb.AddForce (movement * speed);
 	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+    	if (other.gameObject.CompareTag("Pick Up"))
+    	{
+    		other.gameObject.SetActive(false);
+    	}	 
+    }
 }
+
